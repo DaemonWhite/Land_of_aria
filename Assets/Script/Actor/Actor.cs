aff590cde472d1fdfcfadbe7c2966647fe8cf8e7
DaemonWhite <matheo.daemon@gmail.com>
2023-07-14 15:11:13 +0200

    fix access data

--- Assets/Script/Actor/Actor.cs
+++ Assets/Script/Actor/Actor.cs
@@ -2,10 +2,10 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-using tekill;
+using StatsActor;
 
 public class Actor : MonoBehaviour {
-    public StatsActor a = new StatsActor();
+    public BaseStatsActors a = new BaseStatsActors();
 
     void start() {
         a.Strength = 2;
--- Assets/Script/Actor/StatsActor.cs
+++ Assets/Script/Actor/StatsActor.cs
@@ -1,14 +1,19 @@
-namespace tekill {
-    public class StatsActor
+namespace StatsActor {
+    public class BaseStatsActors
     {
 
         public int Strength {get; set;} = 0;
-        private int Luck = 0;
-        private int Inteligence = 0;
-        private int Endurance = 0;
-        private int Charisma = 0;
-        private int Perception = 0;
-        private int Agility = 0;
+        public int Luck {get; set;} = 0;
+        public int Inteligence {get; set;} = 0;
+        public int Endurance{get; set;} = 0;
+        public int Charisma {get; set;} = 0;
+        public int Perception {get; set;} = 0;
+        public int Agility {get; set;} = 0;
+    }
+
+    public class SpecificStatsActors
+    {
+
     }
 
 }
